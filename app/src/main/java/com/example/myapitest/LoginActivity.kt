2b75fe41f6fdf42c04627e1e0f1aa3088c2e46e9
guilhermeFCarvalho <gui.carvalho021@gmail.com>
2024-11-02 01:03:38 -0300

    feat(login) add sms firebase login

---
+++ app/src/main/java/com/example/myapitest/LoginActivity.kt
@@ -0,0 +1,133 @@
+package com.example.myapitest
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapitest.databinding.ActivityLoginBinding
+import com.example.myapitest.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
+    private var verificationId = ""
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        setupView()
+        verifyLoggedUser()
+
+    }
+
+    private fun setupView() {
+        binding.etPhoneNumber.setText("+5511912345678")
+        binding.btnSendCode.setOnClickListener {
+            sendVerificationCode()
+
+        }
+        binding.etVerificationCode.setText("101010")
+        binding.btnVerifyCodeNumber.setOnClickListener {
+            verifyCode()
+        }
+    }
+
+    private fun sendVerificationCode() {
+        val phoneNumber = binding.etPhoneNumber.text.toString()
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+
+                }
+
+                override fun onVerificationFailed(e: FirebaseException) {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "${e.message}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onCodeSent(
+                    verificationId: String,
+                    token: PhoneAuthProvider.ForceResendingToken
+                ) {
+                    this@LoginActivity.verificationId = verificationId
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Código de verificação enviado",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    binding.btnVerifyCodeNumber.visibility = View.VISIBLE
+                    binding.etVerificationCode.visibility = View.VISIBLE
+                }
+
+            })
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private fun verifyCode() {
+        val verificationCode = binding.etVerificationCode.text.toString()
+        val credential = PhoneAuthProvider.getCredential(verificationId, verificationCode)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                onCredentialCompleteListener(task, "Phone Number")
+            }
+    }
+
+    private fun onCredentialCompleteListener(
+        task: Task<AuthResult>,
+        loginType: String
+    ) {
+        if (task.isSuccessful) {
+            val user = auth.currentUser
+            Log.d("LoginActivity", "LoginType: $loginType User: ${user?.uid}")
+            navigateToMainActivity()
+        } else {
+            Toast.makeText(
+                this,
+                "${task.exception?.localizedMessage}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+
+    private fun verifyLoggedUser() {
+        if (auth.currentUser != null && false) {
+            navigateToMainActivity()
+        }
+    }
+
+    private fun navigateToMainActivity() {
+        startActivity(MainActivity.newIntent(this))
+        finish()
+    }
+
+
+}
